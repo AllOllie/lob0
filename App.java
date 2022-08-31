13f4a568dbca93651ee3f2c6c730769e85257892
AllOllie <77171996+AllOllie@users.noreply.github.com>
2022-08-31 11:23:24 -0500

    what

--- App.java
+++ App.java
@@ -10,7 +10,7 @@ public class App extends Application{
     public static void main(String [] args)
     {
         System.out.println("Hello");
-        //System.out.println("there");
+        System.out.println("there");
         launch(args);
        // System.out.println("there");
     }
